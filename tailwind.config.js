051d84f0597aa9bc3cf5066fd31cd9f43a216169
Mario <109123293+darkmariod@users.noreply.github.com>
2024-12-18 16:37:22 -0500

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,astro}", // Asegúrate de incluir los archivos Astro
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
